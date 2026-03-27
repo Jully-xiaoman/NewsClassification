051fc99f91e3e504606e09ae431df299707108ad
CrazyBigBanana <95234746+Jully-xiaoman@users.noreply.github.com>
2026-03-27 20:17:00 +0800

    修改模型加载路径为本地

--- model.py
+++ model.py
@@ -3,6 +3,8 @@ from transformers import BertForSequenceClassification
 def create_model(config):
     model = BertForSequenceClassification.from_pretrained(
         config["model_path"],
-        num_labels=config["num_labels"]
+        num_labels=config["num_labels"],
+        local_files_only=True
     )
     return model
+
